b971db046ffedb0c0adfe7e43f028f0ddc990b05
Babak Bandpey <babak.bandpey@gmail.com>
2020-11-23 01:19:44 -0500

    some leftovers from kali

--- web-app/js/util.js
+++ web-app/js/util.js
@@ -1,4 +1,4 @@
-const DOMAIN = "https://localhost/se/";
+const DOMAIN = "http://localhost/se/";
 const API_KEY = 'Flight%20booker%201.2%20plus%20edition';
 var URL = DOMAIN + 'api/API.php?API_KEY='+API_KEY;
 var URL_STATUS = DOMAIN + 'cgi-bin/status_check?';
